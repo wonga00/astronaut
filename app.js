09d4af54aafc5453aef0171f706c92ad26a43a73
Andrew Wong <wongavision@gmail.com>
2012-12-30 13:16:22 -0800

    compatibility with 0.6.17

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ console.log("Express server listening on port %d in %s mode", server.address().p
 readVideos();
 
 var http = require('http');
-http.get('http://www.google.com', function(res) {
+http.get({host: 'www.google.com', port: 80, path: '/'}, function(res) {
   console.log("Got response: " + res.statusCode);
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
