d1baa50438c1454e5923ed2bbb07afef8cf5dc70
Andrew Wong <wongavision@gmail.com>
2015-01-02 23:53:51 -0800

    fixed shared vid

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ io.sockets.on('connection', function(socket) {
 app.get('/',  function(req, res){
   res.render('index.ejs', {
     host: "http://astronaut.io",
-    vidId: ""
+    vid: ""
   });
 });
 
